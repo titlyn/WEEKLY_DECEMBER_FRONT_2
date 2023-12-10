75bac863a6f283885b5ab8a443b89ee4c90b1fe6
Tolotra Mandresy <tolotramandresy39@gmail.com>
2023-12-10 20:01:48 +0300

    loader done

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,6 @@
-import { NAV_TOGGLER } from "./modules/elements.js";
+import { NAV_TOGGLER, LOADER } from "./modules/elements.js";
 import { toggleMobilNav } from "./modules/nav.js";
+import { endLoader } from "./modules/loader.js";
 
 NAV_TOGGLER.addEventListener('click', toggleMobilNav)
+window.addEventListener('load' , () => endLoader())
